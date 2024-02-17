e29a70bd61e172803186baf87e25c42856e1df95
chorongs <charmblues@gmail.com>
2024-02-17 15:20:49 +0900

    컴포넌트 생성 (조회수 상위 3개 노출)

---
+++ blog-front/src/components/Top3Item/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './style.css';
+import DeFaultProfileImage from '../../assets/image/default-profile-image.png';
+import { BoardListItem } from 'src/types/interface';
+import { useNavigate } from 'react-router-dom';
+
+interface Props {
+  top3ListItem: BoardListItem;
+}
+
+//          component: Top 3 List Item 컴포넌트          //
+export default function Top3Item({ top3ListItem }: Props) {
+  // properties //
+  const { boardNumber, title, content, boardTitleImage } = top3ListItem;
+  const { favoriteCount, commentCount, viewCount } = top3ListItem;
+  const { writeDatetime, writerNickname, writerProfileImage } = top3ListItem;
+
+  //          function: 네비게이트 함수          //
+  //   const navigator = useNavigate();
+
+  //           event handler: 게시물 아이템 클릭 이벤트 처리 방식             //
+  const onClickHandler = () => {
+    // navigator(boardNumber);
+  };
+
+  //          component: Top 3 List Item 컴포넌트  랜더링        //
+  return (
+    <div
+      className='top-3-list-item'
+      style={{ backgroundImage: `url(${boardTitleImage})` }}
+      onClick={onClickHandler}
+    >
+      <div className='top-3-list-item-main-box'>
+        <div className='top-3-list-item-top'>
+          <div className='top-3-list-item-profile-box'>
+            <div
+              className='top-3-list-item-profile-image'
+              style={{
+                backgroundImage: `url(${
+                  writerProfileImage ? writerProfileImage : DeFaultProfileImage
+                })`,
+              }}
+            ></div>
+          </div>
+          <div className='top-3-list-item-write-box'>
+            <div className='top-3-list-item-nickname'>{writerNickname}</div>
+            <div className='top-3-list-item-write-date'>{writeDatetime}</div>
+          </div>
+        </div>
+        <div className='top-3-list-item-middle'>
+          <div className='top-3-list-item-title'>{title}</div>
+          <div className='top-3-list-item-content'>{content}</div>
+        </div>
+        <div className='top-3-list-item-bottom'>
+          <div className='top-3-list-item-counts'>
+            {`댓글 ${commentCount} · 좋아요 ${favoriteCount} · 조회수 ${viewCount}`}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
