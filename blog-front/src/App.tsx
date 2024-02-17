aaf89cd80d774e927459533da6b2cd5718548630
chorongs <charmblues@gmail.com>
2024-02-18 01:21:00 +0900

    SPA

--- blog-front/src/App.tsx
+++ blog-front/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import BoardItem from './components/BoardItem';
 import {
@@ -10,21 +10,13 @@ import {
 import Top3Item from './components/Top3Item';
 import CommentItem from './components/CommentItem';
 import FavoriteItem from './components/FavoriteItem';
+import InputBox from './components/InputBox';
+import Footer from './layouts/Footer';
 
 function App() {
   return (
     <>
-      <div
-        style={{
-          display: 'flex',
-          columnGap: '30px',
-          rowGap: '20px',
-        }}
-      >
-        {favoriteListMock.map((favoriteListItem) => (
-          <FavoriteItem favoriteListItem={favoriteListItem} />
-        ))}
-      </div>
+      <Footer />
     </>
   );
 }
