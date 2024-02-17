618141a79aafc8ab79c8dbb5684da34cecd314fd
chorongs <charmblues@gmail.com>
2024-02-17 13:28:01 +0900

    게시물 아이템 리스트 Front

---
+++ blog-front/src/mocks/latest-board-list.mock.ts
@@ -0,0 +1,59 @@
+import { BoardListItem } from 'src/types/interface';
+
+const latestBoardListMock: BoardListItem[] = [
+  {
+    boardNumber: 1,
+    title: '아 공부하기 싫다.',
+    content: '오늘은 정말 쉬고 싶어...',
+    boardTitleImage:
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDAyMjJfMzQg%2FMDAxNTgyMzU2NTc0NjEz.Kq5JuY6gTT0iRawHpjOKU9qbmpgf_4BhA9gAgql-BmUg.pLJPm-bRSoGtw2IRPz4eR6S5WSF_iUOGTLJvloC9i-kg.JPEG.sdon1222%2FIMG_2037.JPG&type=sc960_832',
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2024.02.17. 13:18:15',
+    writerNickname: '맹준',
+    writerProfileImage:
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2FMjAxODA3MTVfMTU1%2FMDAxNTMxNjQ3OTg2MjI5.lkLeX32Zsv9UiA-12TimuikXheL9ZCl25pGRbQBVRBMg.P5Lc_PJwZC6ghp77teTClQ1fzJiLliLywnZcNK-ZDhUg.JPEG.i__wlaud%2FexternalFile.jpg&type=sc960_832',
+  },
+
+  {
+    boardNumber: 1,
+    title: '아 공부하기 싫다.',
+    content: '오늘은 정말 쉬고 싶어...',
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2024.02.17. 13:18:15',
+    writerNickname: '맹준',
+    writerProfileImage: null,
+  },
+
+  {
+    boardNumber: 1,
+    title: '아 공부하기 싫다.',
+    content: '오늘은 정말 쉬고 싶어...',
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2024.02.17. 13:18:15',
+    writerNickname: '맹준',
+    writerProfileImage: null,
+  },
+
+  {
+    boardNumber: 1,
+    title: '아 공부하기 싫다.',
+    content: '오늘은 정말 쉬고 싶어...',
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2024.02.17. 13:18:15',
+    writerNickname: '맹준',
+    writerProfileImage: null,
+  },
+];
+
+export default latestBoardListMock;
